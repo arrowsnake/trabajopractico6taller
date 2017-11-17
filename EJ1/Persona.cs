2b99ddeb94ebc0bd80c3984eb7481c644757a193
Nazareno Sotto <emanuel_scs@hotmail.com>
2017-11-17 18:44:58 -0300

    comente el coso de persona

---
+++ EJ1/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJ1
+{
+    class Persona
+    {
+        public int PersonaId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public IList<Telefono> Telefonos { get; set; }
+    }
+
+}
