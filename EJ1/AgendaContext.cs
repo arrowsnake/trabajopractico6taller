1dd83ab35a92a91300e6b6a595173f176df6a5af
Nazareno Sotto <emanuel_scs@hotmail.com>
2017-11-17 18:44:58 -0300

    Agregar archivos de proyecto.

---
+++ EJ1/AgendaContext.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace EJ1
+{   
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class AgendaContext : DbContext
+    {
+       public  DbSet<Persona> Personas { get; set; }
+       public DbSet <Telefono> Telefonos { get; set; }
+
+       public AgendaContext():base("MyContext") { }
+    }
+}
